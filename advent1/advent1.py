d0ab6e4c4496eb9acaf5f99f6689515212ce3d69
Mark MacDonald <mark205205@hotmail.com>
2021-12-01 13:48:59 -0800

    first commit

---
+++ advent1/advent1.py
@@ -0,0 +1,36 @@
+# Get data for challenge 
+# ** Could not automate via http request:
+# Url same for everyone but data is different. 
+# There may be some args to add that would provide access
+ 
+dataFile = open('advent1_data.txt', 'r')
+
+dataIntList = []
+for line in dataFile.readlines():
+    dataIntList.append(int(line))
+
+def countIncreases(intList):
+    result = 0
+    for i in range(len(intList)):
+        if i > 0:
+            if intList[i] > intList[i-1]:
+                result += 1
+    return result
+
+print(countIncreases(dataIntList))
+
+def countIncreasesSlidingWindow(intList):
+    result = 0
+    for i in range(len(intList)):
+        # break if at the last window
+        if i == len(intList) - 3:
+            break
+
+        firstWindow = list(intList[ix] for ix in [i, (i+1), (i+2)])
+        secondWindow = list(intList[ix] for ix in [(i+1), (i+2), (i+3)])
+
+        if sum(firstWindow) < sum(secondWindow):
+            result += 1
+    return result
+
+print(countIncreasesSlidingWindow(dataIntList))
